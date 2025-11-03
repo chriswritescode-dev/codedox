58242e7afea120900b69148029d93887b788954a
Chris Scott <chris@chriswritescode.dev>
2025-11-03 10:16:25 -0500

    Bump version to 0.3.5

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 """CodeDox constants and version information."""
 
-__version__ = "0.3.2"
+__version__ = "0.3.5"
 __author__ = "Chris Scott"
 __description__ = "Documentation Code Extraction and Search System"
 __app_name__ = "CodeDox"
